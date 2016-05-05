d39de3ccf49e07f570f2e040b178229157668f17
Ian Wong <ian.w.maniac@hotmail.com>
2016-05-05 14:19:00 -0400

    spacing fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-	before_filter :current_user, :permission_check, :check_if_logged_on
+  before_filter :current_user, :permission_check, :check_if_logged_on
 
-	# addresses that can be accessed while not logged in, but should not be accessed once logged in
-	def outside_path?(param)
-		outside_addresses = [
-			"/login",
-			"/signup",
-			"/"
-		]
-		outside_addresses.any?{ |path| path[param] }
-	end
+  # addresses that can be accessed while not logged in, but should not be accessed once logged in
+  def outside_path?(param)
+    outside_addresses = [
+      "/login",
+      "/signup",
+      "/"
+    ]
+    outside_addresses.any?{ |path| path[param] }
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:session_key])
@@ -29,9 +29,9 @@ class ApplicationController < ActionController::Base
     controller = controller_name
     action = action_name
 		
-		# if logged in, send the user to their home page if trying to access the login or signup pages
-		if outside_path?(request.path) || (controller == "users" && action == "create") 
-			redirect_to @current_user if !current_user.nil?
+    # if logged in, send the user to their home page if trying to access the login or signup pages
+    if outside_path?(request.path) || (controller == "users" && action == "create") 
+      redirect_to @current_user if !current_user.nil?
     elsif !outside_path?(request.path) && !(controller == "users" && action == "create")
       redirect_to :login if current_user.nil?
     end
